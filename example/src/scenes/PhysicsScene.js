9f87483c05dcb910b68a829fe155d2bfd355be95
Broch Stilley <broch@lumi.com>
2016-08-25 17:26:04 -0700

    Small update to PhysicsScene example.

--- example/src/scenes/PhysicsScene.js
+++ example/src/scenes/PhysicsScene.js
@@ -22,28 +22,33 @@ class PhysicsScene extends Component {
     }
 
     render() {
+
+        const boxes = Immutable.Range(0, 160).map((v) => (
+            <Box>
+                <CheckCollisions />
+                <Position vector={[Math.random() * 6, 2, Math.random()]} />
+                <Rotate
+                    axis={[1, 1.9, 1]}
+                    amount={60}
+                    space="LOCAL"
+                />
+                <Material>
+                    <Color color={[0.2, 0.5, 0.9]} />
+                </Material>
+                <PhysicsState mass={3} />
+            </Box>
+        ));
+
         return (
             <Scene>
                 <CollisionsEnabled />
                 <Physics gravity={[0, -10, 0]} />
                 <ArcRotateCamera
-                    position={[-14, 6, -13]}
+                    position={[-14, 24, -16]}
                     target={[0, -3, 0]}
                 />
                 <HemisphericLight />
-                <Box>
-                    <CheckCollisions />
-                    <Position vector={[0, 2, 0]} />
-                    <Rotate
-                        axis={[1, 1.9, 1]}
-                        amount={60}
-                        space="LOCAL"
-                    />
-                    <Material>
-                        <Color color={[0.2, 0.5, 0.9]} />
-                    </Material>
-                    <PhysicsState mass={3} />
-                </Box>
+                {boxes}
                 <Ground
                     height={300}
                     width={300}
